6aa2e74110205040716e7462f006c507bef78651
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-09-11 14:11:09 +0530

    How java works

---
+++ Core Java/HowJavaWorks.java
@@ -0,0 +1,24 @@
+class HowJavaWorks{
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+
+//                                JVM
+//                                 ^
+//                                 |
+//                                 |
+//                                 |
+// Java  ---->   Compiler  ---->  Byte
+// Code           (Javac)         Code
+                                
+// Java is WORA - Write Once Read Anywhere.
+
+// Entry point of code (starting point of execution)
+// public static void main() method
+// JVM is platform dependent
+// JVM --> JAVA VIRTUAL MACHINE
+// JRE --> JAVA RUNTIME ENVIRONMENT
+// JVM is a part of JRE
