db544b7a6cfa4b063dc74d866c338ef7152dd6ac
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-09-11 14:48:45 +0530

    java data types and increment-decrement operators

--- Core Java/Literal.java
+++ Core Java/Literal.java
@@ -7,6 +7,28 @@ public class Literal {
         System.out.println(num1);
         System.out.println(num2);
         System.out.println(num3);
+
+
+        //Data Types in java;
+        byte b = 127; //1 byte
+        short s = 10000; //2 bytes
+        int i = 293923; //4 bytes
+        long l = 390843; //8 bytes
+        float f = 33.4f; //4 bytes
+        double d = 33.4444448949; //8 bytes
+        char c = 'd'; //Unicode 2 bytes
+
+
+        int number = 1;
+        ++number; //(2)pre increment operator - increment the value first then use
+        System.out.println(number); 
+        --number; //(1)pre decrement operator - decrement the value first then use
+        System.out.println(number);
+        number++; //(2)post increment operator - use first then increment the value
+        System.out.println(number);
+        number--; //(1)post decrement operator - use first then decrement the value
+
+
     }
 }
  
