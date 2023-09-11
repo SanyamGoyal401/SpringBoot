16eb5304f39ba451b37bb6cc4fcabe85f0854403
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-09-11 20:00:16 +0530

    basic calculator class

---
+++ Core Java/class-object-theory/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+
+    int a; //member variables
+
+    public int add(){//member functions
+        System.out.println("Add");
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
